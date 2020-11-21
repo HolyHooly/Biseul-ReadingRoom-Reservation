3f0a07c5c10cf109b8da098743908d205c292888
tong3 <amying3612@gmail.com>
2020-11-21 22:12:37 +0900

    Logger class 생성, Logger::write_log 구현

--- Biseul ReadingRoom Reservation/BiseulReadingRoomReservation_log.cpp
+++ Biseul ReadingRoom Reservation/BiseulReadingRoomReservation_log.cpp
@@ -1,1 +1,31 @@
+#include "BiseulReadingRoomReservation_log.h"
 
+void Logger::log_init(){
+    logging::add_file_log
+    (
+        keywords::file_name = "log_%N.log",                                      /*< file name pattern >*/
+        keywords::rotation_size = 1024 * 1024,                                   /*< rotate files every 1 MiB... >*/
+        keywords::max_files = 20,                                                // 파일 수 최대 20개
+        keywords::format = "[%TimeStamp%]: %Message%"                            /*< log record format >*/
+    );
+
+    logging::core::get()->set_filter
+    (
+        logging::trivial::severity >= logging::trivial::info
+    );
+}
+
+void Logger::write_log(std::string msg){
+    logging::add_common_attributes();
+
+    using namespace logging::trivial;
+    src::severity_logger< severity_level > lg;
+
+    BOOST_LOG_SEV(lg, info) << msg;
+}
+
+bool Logger::save_status(std::string)
+{
+    //뭐 받는진 모르겠는데 csv 파일 열 바꾸는 건 string ,로 구분한대 class로 그대로 줘도 될듯~~ 맨위에 저장 시간도 적을 수 있을 듯~~
+    return false;
+}
--- Biseul ReadingRoom Reservation/BiseulReadingRoomReservation_log.h
+++ Biseul ReadingRoom Reservation/BiseulReadingRoomReservation_log.h
@@ -1,1 +1,23 @@
 
+#include "string.h"
+
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+
+class Logger {
+public:
+    void log_init();  // info log 파일 생성 초기 함수
+    void write_log(std::string msg); // info log 작성
+    bool save_status(std::string); // 현재 자리 csv 파일로 저장, string이나 class 전부 받을 수 있을듯!
+};
