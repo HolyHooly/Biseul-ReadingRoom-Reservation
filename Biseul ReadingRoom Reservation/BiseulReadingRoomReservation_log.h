70267f54cbef6dd6425056986ea51b2725eab42c
tong3 <amying3612@gmail.com>
2020-11-21 22:31:00 +0900

    Logger class -> log 함수

--- Biseul ReadingRoom Reservation/BiseulReadingRoomReservation_log.h
+++ Biseul ReadingRoom Reservation/BiseulReadingRoomReservation_log.h
@@ -20,9 +20,37 @@ namespace src = boost::log::sources;
 namespace sinks = boost::log::sinks;
 namespace keywords = boost::log::keywords;
 
-class Logger {
-public:
-    void log_init();  // info log 파일 생성 초기 함수
-    void write_log(std::string); // info log 작성
-    bool save_status(std::string); // 현재 자리 csv 파일로 저장, string이나 class 전부 받을 수 있을 듯! parameter주면 알아서 구현하겠음
-};
+void log_init();  // info log 파일 생성 초기 함수
+void write_log(std::string); // info log 작성
+bool save_status(std::string); // 현재 자리 csv 파일로 저장, string이나 class 전부 받을 수 있을 듯! parameter주면 알아서 구현하겠음
+
+
+void log_init() {
+    logging::add_file_log
+    (
+        keywords::file_name = "log_%N.log",                                      /*< file name pattern >*/
+        keywords::rotation_size = 1024 * 1024,                                   /*< rotate files every 1 MiB... >*/
+        keywords::max_files = 20,                                                // 파일 수 최대 20개
+        keywords::format = "[%TimeStamp%]: %Message%"                            /*< log record format >*/
+    );
+
+    logging::core::get()->set_filter
+    (
+        logging::trivial::severity >= logging::trivial::info
+    );
+}
+
+void write_log(std::string msg) {
+    logging::add_common_attributes();
+
+    using namespace logging::trivial;
+    src::severity_logger< severity_level > lg;
+
+    BOOST_LOG_SEV(lg, info) << msg;
+}
+
+bool save_status(std::string)
+{
+    //뭐 받는진 모르겠는데 csv 파일 열 바꾸는 건 string ,로 구분한대 class로 그대로 줘도 될듯~~ 맨위에 저장 시간도 적을 수 있을 듯~~
+    return false;
+}
