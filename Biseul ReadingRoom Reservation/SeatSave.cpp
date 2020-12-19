6d080643dd7a210950f8da0eab3f3b6900bca2d9
tong3 <amying3612@gmail.com>
2020-12-20 02:01:26 +0900

    error check

--- Biseul ReadingRoom Reservation/SeatSave.cpp
+++ Biseul ReadingRoom Reservation/SeatSave.cpp
@@ -14,7 +14,7 @@ namespace biseul_rroom {
             }
             UserManger* manager = nullptr;
 
-            int seat_no = seat_info[i].first;
+            int seat_no = seat_info[i].first + 1;
             std::string name = seat_info[i].second->get_reserver()->get_name();
             int student_id = seat_info[i].second->get_reserver()->get_stud_id();
             __int64 rfid_id = seat_info[i].second->get_reserver()->get_rfid_id();
@@ -82,44 +82,45 @@ namespace biseul_rroom {
                 word[i] = strtok_s(NULL, ",", &context);
             }
 
-            //예약한 시간, 예약 끝 시간 각각 tm에 넣어주기
-            char* r_time[9] = { nullptr, }, * rcon;
-
-            r_time[0] = strtok_s(word[6], "-", &rcon);
-            for (int i = 1; i < 8; i++) {
-                r_time[i] = strtok_s(NULL, "-", &rcon);
-            }
-            tm reserved = {0,};
-            reserved.tm_hour = atoi(r_time[0]);
-            reserved.tm_min = atoi(r_time[1]);
-            reserved.tm_sec = atoi(r_time[2]);
-            reserved.tm_mday = atoi(r_time[3]);
-            reserved.tm_mon = atoi(r_time[4]);
-            reserved.tm_year = atoi(r_time[5]);
-            reserved.tm_wday = atoi(r_time[6]);
-            reserved.tm_yday = atoi(r_time[7]);
-            reserved.tm_isdst = atoi(r_time[8]);
-
-            char* re_time[9] = { nullptr, }, * recon;
-
-            re_time[0] = strtok_s(word[7], "-", &recon);
-            for (int i = 1; i < 8; i++) {
-                re_time[i] = strtok_s(NULL, "-", &recon);
-            }
-            tm reserve_end = { 0, };
-            reserve_end.tm_hour = atoi(re_time[0]);
-            reserve_end.tm_min = atoi(re_time[1]);
-            reserve_end.tm_sec = atoi(re_time[2]);
-            reserve_end.tm_mday = atoi(re_time[3]);
-            reserve_end.tm_mon = atoi(re_time[4]);
-            reserve_end.tm_year = atoi(re_time[5]);
-            reserve_end.tm_wday = atoi(re_time[6]);
-            reserve_end.tm_yday = atoi(re_time[7]);
-            reserve_end.tm_isdst = atoi(re_time[8]);
-
-
             // seat number 유무 따라서 seat에 채워주기
             if (seat_number_checker != 0) {
+
+                //예약한 시간, 예약 끝 시간 각각 tm에 넣어주기
+                char* r_time[9] = { nullptr, }, * rcon;
+
+                r_time[0] = strtok_s(word[6], "-", &rcon);
+                for (int i = 1; i < 9; i++) {
+                    r_time[i] = strtok_s(NULL, "-", &rcon);
+                }
+                tm reserved = { 0, };
+                reserved.tm_hour = atoi(r_time[0]);
+                reserved.tm_min = atoi(r_time[1]);
+                reserved.tm_sec = atoi(r_time[2]);
+                reserved.tm_mday = atoi(r_time[3]);
+                reserved.tm_mon = atoi(r_time[4]);
+                reserved.tm_year = atoi(r_time[5]);
+                reserved.tm_wday = atoi(r_time[6]);
+                reserved.tm_yday = atoi(r_time[7]);
+                reserved.tm_isdst = atoi(r_time[8]);
+
+                char* re_time[9] = { nullptr, }, * recon;
+
+                re_time[0] = strtok_s(word[7], "-", &recon);
+                for (int i = 1; i < 9; i++) {
+                    re_time[i] = strtok_s(NULL, "-", &recon);
+                }
+                tm reserve_end = { 0, };
+                reserve_end.tm_hour = atoi(re_time[0]);
+                reserve_end.tm_min = atoi(re_time[1]);
+                reserve_end.tm_sec = atoi(re_time[2]);
+                reserve_end.tm_mday = atoi(re_time[3]);
+                reserve_end.tm_mon = atoi(re_time[4]);
+                reserve_end.tm_year = atoi(re_time[5]);
+                reserve_end.tm_wday = atoi(re_time[6]);
+                reserve_end.tm_yday = atoi(re_time[7]);
+                reserve_end.tm_isdst = atoi(re_time[8]);
+
+                
                 if (atoi(word[2]) != 0) {
                     SeatStatus ss = seat_status_converter(std::string(word[4]));
                     Reserver r = Reserver(std::string(word[1]), atoi(word[2]), _atoi64(word[3]));
